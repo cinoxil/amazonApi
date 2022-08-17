fc8f62191064f91369a53b123bba8d4c37a919ab
cinoxil <54507093+cinoxil@users.noreply.github.com>
2022-08-18 00:05:47 +0300

    ss

--- controller/api.js
+++ controller/api.js
@@ -1,32 +1,9 @@
 const axios = require('axios');
-const { readFile } = require('./helper');
+const { readFile, apiReq } = require('./helper');
 
 module.exports = {
 	getDataFromApi: async (req, res) => {
-		var asins = await readFile();
-		if (!asins) return res.send().status({ message: 'File is empty.' });
-		let paramsUSA = {};
-		let paramsCA = {};
-		let resultUSA = [];
-		let resultCA = [];
-		asins.forEach(async (element) => {
-			paramsUSA = {
-				api_key: '35DB182C051A4D338AB427DAA69A274A',
-				amazon_domain: 'amazon.com',
-				asin: element,
-				type: 'product',
-			};
-			paramsCA = {
-				api_key: '35DB182C051A4D338AB427DAA69A274A',
-				amazon_domain: 'amazon.ca',
-				asin: element,
-				type: 'product',
-			};
-			var aMERICA = await axios.get('https://api.rainforestapi.com/request', { paramsUSA });
-
-			var cANADA = await axios.get('https://api.rainforestapi.com/request', { paramsCA });
-		});
-
+		var [resultUsa, resultCa] = apiReq();
 		res.json(resultUSA, resultCA);
 	},
 };
--- controller/helper.js
+++ controller/helper.js
@@ -9,6 +9,37 @@ async function readFile() {
 	return rows;
 }
 
+async function apiReq() {
+	var asins = await readFile();
+	console.log(asins.length);
+	if (!asins) return res.send().status({ message: 'File is empty.' });
+	let paramsUsa = {};
+	let paramsCa = {};
+	asins.forEach(async (element) => {
+		paramsUsa = {
+			api_key: '35DB182C051A4D338AB427DAA69A274A',
+			amazon_domain: domain,
+			asin: 'amazon.com',
+			type: 'product',
+		};
+		paramsCa = {
+			api_key: '35DB182C051A4D338AB427DAA69A274A',
+			amazon_domain: domain,
+			asin: 'amazon.ca',
+			type: 'product',
+		};
+		var [usa, ca] = await axios.all([
+			'https://api.rainforestapi.com/request',
+			{ paramsUsa },
+			'https://api.rainforestapi.com/request',
+			{ paramsCa },
+		]);
+	});
+
+	return usa, ca;
+}
+
 module.exports = {
 	readFile,
+	apiReq,
 };

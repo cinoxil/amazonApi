0283ad09150f236c266b57a0d8f24f5717b36eee
cinoxil <54507093+cinoxil@users.noreply.github.com>
2022-08-21 19:58:15 +0300

    ss

--- controller/api.js
+++ controller/api.js
@@ -1,8 +1,19 @@
-const axios = require('axios');
-const { readFile, apiReqUsa, apiReqCa } = require('./helper');
+const { apiReq, readFile, compareProducts } = require('./helper');
 
 module.exports = {
 	getDataFromApi: async (req, res) => {
-		const [usa, ca] = await Promise.all([apiReqUsa(), apiReqCa()]);
+		const file = await readFile();
+		const [usa, ca] = await Promise.all([apiReq('amazon.com', file), apiReq('amazon.ca', file)]);
+
+		res.send(compareProducts(usa, ca, 2, false, 4));
+
+		// console.log(usa.length);
+		// console.log(ca.length);
+		// const siktim = [];
+		// usa.forEach((element, index) => {
+		// 	siktim.push({ usa: element.data, ca: ca[index].data });
+		// });
+
+		// return res.send(siktim);
 	},
 };
--- controller/helper.js
+++ controller/helper.js
@@ -9,47 +9,81 @@ async function readFile() {
 	}
 	return rows;
 }
-async function apiReqUsa() {
-	var asins = await readFile();
-
+async function apiReq(domain, asins) {
 	if (!asins) return res.send().status({ message: 'File is empty.' });
 
-	var usa = [];
+	var data = [];
 
 	for (let i = 0; i < asins.length; i++) {
 		const params = {
 			api_key: '35DB182C051A4D338AB427DAA69A274A',
-			amazon_domain: 'amazon.com',
+			amazon_domain: domain,
 			asin: asins[i],
 			type: 'product',
 		};
-		usa[i] = await axios.get('https://api.rainforestapi.com/request', { params });
+		data[i] = await axios.get('https://api.rainforestapi.com/request', { params });
 	}
 
-	return usa;
+	return data;
 }
-async function apiReqCa() {
-	var asins = await readFile();
+function compareProducts(listUsa, listCa, ratio, fbaStatus, rating) {
+	var filteredProducts = [];
 
-	if (!asins) return res.send().status({ message: 'File is empty.' });
+	listUsa.forEach((element, index) => {
+		// console.log(usaSuccessValue);
+		// console.log(usaPriceValue);
+		// console.log(usaInStock);
+		// console.log(usaIsFulfilledByAmazon);
+		// console.log(usaRating);
+		// console.log('--------------------------------');
+		// console.log(caSuccessValue);
+		// console.log(caPriceValue);
+		// console.log(caInStock);
+		// console.log(caIsFulfilledByAmazon);
+		// console.log(caRating);
+		try {
+			var usaSuccessValue = element.data.request_info.success;
+			var usaPriceValue = element.data.product.buybox_winner.price.value;
+			var usaInStock = element.data.product.buybox_winner.availability.type;
+			var usaIsFulfilledByAmazon = element.data.product.buybox_winner.fulfillment.is_fulfilled_by_amazon;
+			var usaRating = element.data.product.rating;
 
-	var ca = [];
+			var caSuccessValue = listCa[index].data.request_info.success;
+			var caPriceValue = listCa[index].data.product.buybox_winner.price.value;
+			var caInStock = listCa[index].data.product.buybox_winner.availability.type;
+			var caIsFulfilledByAmazon = listCa[index].data.product.buybox_winner.fulfillment.is_fulfilled_by_amazon;
+			var caRating = listCa[index].data.product.rating;
 
-	for (let i = 0; i < asins.length; i++) {
-		const params = {
-			api_key: '35DB182C051A4D338AB427DAA69A274A',
-			amazon_domain: 'amazon.ca',
-			asin: asins[i],
-			type: 'product',
-		};
-		ca[i] = await axios.get('https://api.rainforestapi.com/request', { params });
-	}
+			var priceRatio = caPriceValue / usaPriceValue;
+			if (
+				usaSuccessValue == true &&
+				caSuccessValue == true &&
+				priceRatio > ratio &&
+				usaInStock == 'in_stock' &&
+				usaIsFulfilledByAmazon == true &&
+				caIsFulfilledByAmazon == fbaStatus &&
+				caRating >= rating
+			) {
+				filteredProducts.push({
+					usaLink: element.data.product.link,
+					caLink: listCa[index].data.product.link,
+					asin: element.data.product.asin,
+					priceRatio: priceRatio,
+					rating: listCa[index].data.product.rating,
+					buyingPrice: usaPriceValue,
+					sellingPrice: caPriceValue,
+				});
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	});
 
-	return ca;
+	return filteredProducts;
 }
 
 module.exports = {
 	readFile,
-	apiReqUsa,
-	apiReqCa,
+	apiReq,
+	compareProducts,
 };

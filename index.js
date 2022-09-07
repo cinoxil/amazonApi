be0fadf271b73a7188261748d7d1fd6b616f204c
bfzyazilim <bfzyazilim@192.168.1.103>
2022-09-07 14:02:11 +0300

    ss

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const { authController } = require("./controller/auth");
 var cors = require("cors");
 const { auhtMw } = require("./middleware/auth");
 const { userMw } = require("./middleware/user");
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.post("/login", auhtMw.signIn, authController.signIn);

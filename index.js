437de4e3e2b5b4eb0f7ee262631bc9bef15c1570
bfzyazilim <bfzyazilim@192.168.1.103>
2022-09-07 15:08:23 +0300

    ss

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { auhtMw } = require("./middleware/auth");
 const { userMw } = require("./middleware/user");
 const corsOptions = {
   origin: "*",
-  credentials: true, //access-control-allow-credentials:true
+  credentials: false, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));

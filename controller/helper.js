b918f7063f4f7e27e018cd52b0e13faceba8353f
cinoxil <54507093+cinoxil@users.noreply.github.com>
2022-09-30 16:35:50 +0300

    helper.js
    compareProducts try/catch

--- controller/helper.js
+++ controller/helper.js
@@ -41,94 +41,97 @@ async function apiReq(domain, asins) {
 	return data;
 }
 function compareProducts(listUsa, listCa, ratio, fbaStatus, rating) {
-	return new Promise((resolve, reject) => {
-		const options = {
-			method: 'GET',
-			url: 'https://currencyscoop.p.rapidapi.com/latest',
-			headers: {
-				'X-RapidAPI-Key': 'e370a032c5mshcfc920f383bd638p198060jsnf2e7230051fc',
-				'X-RapidAPI-Host': 'currencyscoop.p.rapidapi.com',
-			},
-		};
+	try {
+		return new Promise((resolve, reject) => {
+			const options = {
+				method: 'GET',
+				url: 'https://currencyscoop.p.rapidapi.com/latest',
+				headers: {
+					'X-RapidAPI-Key': 'e370a032c5mshcfc920f383bd638p198060jsnf2e7230051fc',
+					'X-RapidAPI-Host': 'currencyscoop.p.rapidapi.com',
+				},
+			};
+			axios
+				.request(options)
+				.then(function (response) {
+					var filteredProducts = [];
+					listUsa.forEach((element, index) => {
+						try {
+							var usaSuccessValue = element.data?.request_info?.success;
 
-		axios
-			.request(options)
-			.then(function (response) {
-				var filteredProducts = [];
-				listUsa.forEach((element, index) => {
-					try {
-						var usaSuccessValue = element.data?.request_info?.success;
+							var usaPriceValue = parseFloat(element.data?.product?.buybox_winner?.price?.value);
+							var usaInStock = element.data?.product?.buybox_winner?.availability?.type;
+							var usaIsFulfilledByAmazon =
+								element.data?.product?.buybox_winner?.fulfillment?.is_fulfilled_by_amazon;
+							//var usaRating = element.data.product.rating;
 
-						var usaPriceValue = parseFloat(element.data?.product?.buybox_winner?.price?.value);
-						var usaInStock = element.data?.product?.buybox_winner?.availability?.type;
-						var usaIsFulfilledByAmazon =
-							element.data?.product?.buybox_winner?.fulfillment?.is_fulfilled_by_amazon;
-						//var usaRating = element.data.product.rating;
+							var caSuccessValue = listCa[index].data?.request_info?.success;
+							var caPriceValue = parseFloat(listCa[index].data?.product?.buybox_winner?.price?.value);
+							//var caInStock = listCa[index].data.product.buybox_winner.availability.type;
+							var caIsFulfilledByAmazon =
+								listCa[index].data?.product?.buybox_winner?.fulfillment?.is_fulfilled_by_amazon;
+							var caRating = parseFloat(listCa[index].data?.product?.rating);
 
-						var caSuccessValue = listCa[index].data?.request_info?.success;
-						var caPriceValue = parseFloat(listCa[index].data?.product?.buybox_winner?.price?.value);
-						//var caInStock = listCa[index].data.product.buybox_winner.availability.type;
-						var caIsFulfilledByAmazon =
-							listCa[index].data?.product?.buybox_winner?.fulfillment?.is_fulfilled_by_amazon;
-						var caRating = parseFloat(listCa[index].data?.product?.rating);
+							var caPrice = caPriceValue / parseFloat(response.data.response.rates.CAD);
+							var priceRatio = caPrice / usaPriceValue;
 
-						var caPrice = caPriceValue / parseFloat(response.data.response.rates.CAD);
-						var priceRatio = caPrice / usaPriceValue;
-
-						if (fbaStatus == true) {
-							if (
-								usaSuccessValue == true &&
-								caSuccessValue == true &&
-								priceRatio > ratio &&
-								usaInStock == 'in_stock' &&
-								usaIsFulfilledByAmazon == true &&
-								caRating >= rating
-							) {
-								filteredProducts.push({
-									usaLink: element.data.product.link,
-									caLink: listCa[index].data.product.link,
-									asin: element.data.product.asin,
-									priceRatio: priceRatio,
-									rating: listCa[index].data.product.rating,
-									buyingPrice: usaPriceValue,
-									sellingPrice: caPrice,
-								});
+							if (fbaStatus == true) {
+								if (
+									usaSuccessValue == true &&
+									caSuccessValue == true &&
+									priceRatio > ratio &&
+									usaInStock == 'in_stock' &&
+									usaIsFulfilledByAmazon == true &&
+									caRating >= rating
+								) {
+									filteredProducts.push({
+										usaLink: element.data.product.link,
+										caLink: listCa[index].data.product.link,
+										asin: element.data.product.asin,
+										priceRatio: priceRatio,
+										rating: listCa[index].data.product.rating,
+										buyingPrice: usaPriceValue,
+										sellingPrice: caPrice,
+									});
+								}
+							} else {
+								if (
+									usaSuccessValue == true &&
+									caSuccessValue == true &&
+									priceRatio > ratio &&
+									usaInStock == 'in_stock' &&
+									usaIsFulfilledByAmazon == true &&
+									caIsFulfilledByAmazon == fbaStatus &&
+									caRating >= rating
+								) {
+									filteredProducts.push({
+										usaLink: element.data.product.link,
+										caLink: listCa[index].data.product.link,
+										asin: element.data.product.asin,
+										priceRatio: priceRatio,
+										rating: listCa[index].data.product.rating,
+										buyingPrice: usaPriceValue,
+										sellingPrice: caPrice,
+									});
+								}
 							}
-						} else {
-							if (
-								usaSuccessValue == true &&
-								caSuccessValue == true &&
-								priceRatio > ratio &&
-								usaInStock == 'in_stock' &&
-								usaIsFulfilledByAmazon == true &&
-								caIsFulfilledByAmazon == fbaStatus &&
-								caRating >= rating
-							) {
-								filteredProducts.push({
-									usaLink: element.data.product.link,
-									caLink: listCa[index].data.product.link,
-									asin: element.data.product.asin,
-									priceRatio: priceRatio,
-									rating: listCa[index].data.product.rating,
-									buyingPrice: usaPriceValue,
-									sellingPrice: caPrice,
-								});
-							}
+						} catch (err) {
+							console.log(err);
+							logError(err);
+							reject(err);
 						}
-					} catch (err) {
-						console.log(err);
-						logError(err);
-						reject(err);
-					}
+					});
+					resolve(filteredProducts);
+				})
+				.catch(function (error) {
+					console.log(error);
+					logError(error);
+					reject(error);
 				});
-				resolve(filteredProducts);
-			})
-			.catch(function (error) {
-				console.log(error);
-				logError(error);
-				reject(error);
-			});
-	});
+		});
+	} catch (error) {
+		logError(error);
+	}
 }
 
 module.exports = {

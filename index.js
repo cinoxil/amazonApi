48bd7b767abb0ba459f85f71f3ac01ae56aecd22
cinoxil <54507093+cinoxil@users.noreply.github.com>
2022-10-12 20:22:47 +0300

    cors options

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const api = require("./controller/api");
-const { authController } = require("./controller/auth");
-var cors = require("cors");
-const { auhtMw } = require("./middleware/auth");
-const { userMw } = require("./middleware/user");
-const corsOptions = {
-  origin: "*",
-  credentials: false, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+const api = require('./controller/api');
+const { authController } = require('./controller/auth');
+var cors = require('cors');
+const { auhtMw } = require('./middleware/auth');
+const { userMw } = require('./middleware/user');
+//const corsOptions = {
+//  origin: "*",
+//  credentials: false, //access-control-allow-credentials:true
+//  optionSuccessStatus: 200,
+//};
+app.use(cors());
 
 app.use(express.json());
-app.post("/login", auhtMw.signIn, authController.signIn);
-app.post("/asins", auhtMw.authenticated, api.getDataFromApi);
-app
-  .route("/users", auhtMw.authenticated, auhtMw.isSuperUser)
-  .get(authController.get)
-  .post(userMw.create, authController.create)
-  .put(userMw.update, authController.update);
-app.post(
-  "/users/:email",
-  auhtMw.authenticated,
-  auhtMw.isSuperUser,
-  authController.delete
-);
+app.post('/login', auhtMw.signIn, authController.signIn);
+app.post('/asins', auhtMw.authenticated, api.getDataFromApi);
+app.route('/users', auhtMw.authenticated, auhtMw.isSuperUser)
+	.get(authController.get)
+	.post(userMw.create, authController.create)
+	.put(userMw.update, authController.update);
+app.post('/users/:email', auhtMw.authenticated, auhtMw.isSuperUser, authController.delete);
 app.listen(3000, () => {
-  console.log(`3000 port'undan server ayaga kaldirildi`);
+	console.log(`3000 port'undan server ayaga kaldirildi`);
 });
